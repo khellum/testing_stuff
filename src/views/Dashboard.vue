a8a9f6e2512e02b5225c4e920c837d3ed994af4d
khellum <khellum.dev@gmail.com>
2022-04-08 08:00:34 +0300

    first commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard">
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'dashboard',
+  
+}
+</script>
